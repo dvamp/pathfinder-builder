bdb4015f359681c5a0c77578b2eb45937d17048f
dvamp <dvamps@gmail.com>
2011-09-09 07:51:00 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PathfinderWorld::Application.config.secret_token = '836e351cd7b8359a568d0dd11bc3b1734af989b07ca2eb68770a0078bc96dcdd324d15a544437aa1f31af3a6169c50bbfa9fd2e010008bfcc2da247a58a16483'
